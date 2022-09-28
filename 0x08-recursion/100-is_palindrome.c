6771e08415a4ddc2441ed52690a43def7e64462a
Ashdof <mzigah4@gmail.com>
2022-09-28 12:07:38 -0700

    Task 7 - a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,55 +1,61 @@
 #include "main.h"
 
-int _strlen_recursion(char *s);
-int helper_palindrome(char *s, int len);
+int _check(char *s, int len);
 
 /**
- * is_palindrome - a function that determines if a string is palindrome
- * @s: the given string
+ * is_palindrome - returns 1 if string is palindrome & 0 if not
+ * @s: takes in a string
  *
- * Return: 1 if string is palindrome and 0 if otherwise
+ * Return: return check if palindrome function
  */
 
 int is_palindrome(char *s)
 {
-	if (!*s)
-		return (0);
+	int length;
 
-	return (_strlen_recursion(s + 1) + 1);
-}
+	length = _strlen_recursion(s);
 
-/**
- * helper_palindrome - A function with a revesed string
- * @s: An input string
- * @len: the length of the string s
- * Return: A reverse string
- */
-
-int helper_palindrome(char *s, int len)
-{
-	if (len <= 1)
-		return (1);
-	else if (*s == *(s + len - 1))
+	if (length <= 1)
 	{
-		return (helper_palindrome(s + 1, len - 2));
+		return (1);
 	}
-	else
-		return (0);
+
+	return (_check(s, length));
 }
 
 /**
- * _strlen_recursion - return length of string
- * @s: the string
+ * _strlen_recursion - string length
+ * @s: takes in string
  *
- * Return: the length of the string
+ * Return: return string length func
  */
 
 int _strlen_recursion(char *s)
 {
-	if (!*s)
+	if (*s == '\0')
 	{
 		return (0);
 	}
 
-	return (_strlen_recursion(s + 1) + 1);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * _check - checks if palindrome
+ * @s: takes in string
+ * @len: length
+ * Return: check if palindrome
+ */
+
+int _check(char *s, int len)
+{
+	if (len <= 1)
+	{
+		return (1);
+	}
+
+	if (*s != s[len - 1])
+		return (0);
+	else
+		return (_check(s + 1, len - 2));
 }
