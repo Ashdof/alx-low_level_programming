7266f0e5481dc5ec2523753712e9b367c7922e5f
Ashdof <mzigah4@gmail.com>
2022-09-28 07:59:07 -0700

    Task 6 - funtion to print 1 if given number is prime and 0 otherwise

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int wrapper(int x, int y);
+
+/**
+ * is_prime_number - returns 1 if number is prime and 0 if number not prime
+ * @n: the number to test for prime
+ *
+ * Return: 1 if prime and 0 if not prime
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 0 || n == 1)
+		return (1);
+	else
+		return (wrapper(2, n));
+}
+
+/**
+ * wrapper - function to determine if number is prime
+ * @n: the number
+ * @x a test number
+ *
+ * Return: compared values
+ */
+
+int wrapper (int x, int n)
+{
+	if (n == x)
+	{
+		return (1);
+	}
+	else if (n % x == 0)
+	{
+		return (0);
+	}
+
+	return (wrapper(x + 1, n));
+}
