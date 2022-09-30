8cfb378209714b4995c0efaac0437229052e6c02
Ashdof <mzigah4@gmail.com>
2022-09-30 11:32:25 -0700

    Task 0 - a function that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,14 +8,9 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 0)
-	{
-		printf("No argument supplied\n");
-	}
-	else
-	{
-		printf("%s\n", argv[0]);
-	}
+	int j = argc - argc;
+
+	printf("%s\n", argv[j]);
 
 	return (0);
 }
