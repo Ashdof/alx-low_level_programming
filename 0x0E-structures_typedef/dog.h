3cce940221c047b0ab404c03a2b44dd2a6460c09
Ashdof <mzigah4@gmail.com>
2022-10-10 15:20:23 +0000

    Task 0 - created a header file for dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,22 @@
-#include "dog.h"
-#include <stdlib.h>
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * init_dog - initialize dog struct
- * @d: struct dog to initialise
- * @name: name of dog
- * @age: age of dog
- * @owner: string
- *
- * Return: NUll or newdog
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
  */
-
-void *init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d == NULL)
-		return;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
