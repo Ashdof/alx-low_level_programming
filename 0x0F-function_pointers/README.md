cdbc9ed922fba49e878171d225e8af4ec226592c
Ashdof <mzigah4@gmail.com>
2022-10-12 15:35:10 +0000

    Task 0 -  a function that prints a name

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+# 0x0F. C - Function pointers
+
+The 16th lesson of the training programme on C Language. In this leson, we explore pointers to functions.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+#endif /* HEADER_FILE */
