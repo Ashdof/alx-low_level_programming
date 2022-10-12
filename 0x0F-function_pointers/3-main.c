43bd0b93d111acb7198ded9916b85456fa753985
Ashdof <mzigah4@gmail.com>
2022-10-12 16:21:13 +0000

    Task 3 - updated the main file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,31 +1,40 @@
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * main - program to perform simple operations
- * @argc: argument count
- * @argv: array of arguments
- * Return: exits 0 normally, 98 on argc error, 99 on illegal operator
+ * main - main function
+ * @argc: argument c
+ * @argv: command line arguments
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-	int (*func)(int, int);
+	int a, b;
+	int (*o)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	func = get_op_func(argv[2]);
-	if (func == NULL)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	o = get_op_func(argv[2]);
+	if (o == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", o(a, b));
+
 	return (0);
 }
