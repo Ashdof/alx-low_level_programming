a4c3b2db409edbaa58b27bcc3f3869e91306ba37
Ashdof <mzigah4@gmail.com>
2022-12-12 16:24:09 +0000

    Created a dynamic library that contains all listed functions

---
+++ 0x18-dynamic_libraries/0-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy one pointer to another
+ * @dest: varied to be copied to
+ * @src: variable to copy
+ *
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, count = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		count++;
+	}
+	dest[count] = src[count];
+
+	return (dest);
+}
