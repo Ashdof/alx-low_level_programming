c25854d92e7400daee200beb9c5100d98565d7a8
Ashdof <mzigah4@gmail.com>
2023-01-06 07:16:33 +0000

    Task 6 - Created a function that deletes a hash table and updated the header file with its prototype

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,7 +34,7 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 
-
+void hash_table_delete(hash_table_t *ht);
 void hash_table_print(const hash_table_t *ht);
 unsigned long int hash_djb2(const unsigned char *str);
 hash_table_t *hash_table_create(unsigned long int size);
