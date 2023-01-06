34bca2e61b74b70254bee8873cb382e89e8fb67d
Ashdof <mzigah4@gmail.com>
2023-01-06 07:07:38 +0000

    created  a function that prints a hash table and updated the header file with it's prototype

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,6 +35,7 @@ typedef struct hash_table_s
 } hash_table_t;
 
 
+void hash_table_print(const hash_table_t *ht);
 unsigned long int hash_djb2(const unsigned char *str);
 hash_table_t *hash_table_create(unsigned long int size);
 char *hash_table_get(const hash_table_t *ht, const char *key);
