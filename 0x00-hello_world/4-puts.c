b72b61ca4af000f9a0877bc8462aedd2b7696518
Ashdof <mzigah4@gmail.com>
2023-10-05 14:42:24 +0000

    Task 4 - a C program that prints a text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char text[] = "\"Programming is like building a multilingual \
+		       puzzle\n";
+
+	puts(text);
+
+	return (0);
+}
