4fb19c11cf04eca64b951469ee8b9a402a2c5989
Ashdof <mzigah4@gmail.com>
2023-10-06 15:02:54 +0000

    Task 2 - a program that prints the alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets
+ *
+ * description: this program prints the alphabets in lowercase, using
+ * the putchar() function
+ *
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
