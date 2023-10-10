42dfa4c59f7a6ab82ae679c94d4d7e39b80bb679
Ashdof <mzigah4@gmail.com>
2023-10-10 20:30:06 +0000

    Task 4 - a function that checks for an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabet
+ * c: the character to check
+ *
+ * description: this function checks whether its argument is an
+ * alphabet or not
+ *
+ * Return: 1 if c is alphabet, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+
+	return (0);
+}
