3d4a4a6cf106d13b002c528f064495771005fab9
Ashdof <mzigah4@gmail.com>
2023-10-16 14:25:58 +0000

    Task 0 - a function that resets an integer number to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset number
+ * @n: a pointer to the integer number to reset
+ *
+ * description: this function resets a number to 98
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int *p = n;
+
+	*p = 98;
+}
