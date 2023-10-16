21228926d8486b0d24faa4925af3af7f8f382a2c
Ashdof <mzigah4@gmail.com>
2023-10-16 18:13:17 +0000

    Task 9 - fix code for wrong output

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	char *p = src;
+	int i;
 
-	while (*p++)
-		*dest++ = *p;
-	*dest = '\0';
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
 
 	return (dest);
 }
