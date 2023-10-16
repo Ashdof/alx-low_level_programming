9f677edb3e17c76333d8f9e70e2688f4f3be1115
Ashdof <mzigah4@gmail.com>
2023-10-16 17:39:20 +0000

    Task 6 - fix wrong output

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,12 +13,11 @@
 void puts2(char *str)
 {
 	int i = 0;
-	char *p = str;
 
-	while (*(p + i) != '\0')
+	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
-			_putchar(*p);
+			_putchar(str[i]);
 
 		i++;
 	}
