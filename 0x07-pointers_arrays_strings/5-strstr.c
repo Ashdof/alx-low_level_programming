bced4ad0f603c991518a3e14895a4205cc9f3b24
Ashdof <mzigah4@gmail.com>
2023-10-24 06:46:24 +0000

    Task 5 - fix bug

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,15 +21,18 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack)
 	{
 		i = 0;
+
 		if (haystack[i] == needle[i])
 		{
 			do
 			{
 				if (needle[i + 1] == '\0')
 					return (haystack);
+
 				i++;
 			} while (haystack[i] == needle[i]);
 		}
+
 		haystack++;
 	}
 
