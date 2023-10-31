7bef5279b9b75b107d36c5a6f8710ab434a4dc15
Ashdof <mzigah4@gmail.com>
2023-10-31 22:06:11 +0000

    Task 3 - a function that initialises a 2 dimensional grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - allocate memory for 2 dimensional array
+ * @width: the width of the grid
+ * @height: the height of the grid
+ *
+ * description: this function allocates space in memory for a grid
+ * of arrays
+ *
+ * Return: a pointer to the grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **mem;
+
+	if ((width <= 0) || (height <= 0))
+		return (NULL);
+
+	mem = malloc(sizeof(int) * height);
+	if (mem == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		mem[i] = malloc(sizeof(int) * width);
+		if (mem[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(mem[i]);
+			free(mem);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			mem[i][j] = 0;
+	}
+
+	return (mem);
+}
