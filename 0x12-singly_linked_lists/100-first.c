bb4af3aca73e90fa918a07969861d92819343599
Ashdof <mzigah4@gmail.com>
2023-11-16 17:33:39 +0000

    Task 5 - fix requirement issue

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,6 +13,6 @@ void executeBeforeMain(void) __attribute__((constructor));
 
 void executeBeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore "
-		"my house on my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house on my back!\n");
 }
