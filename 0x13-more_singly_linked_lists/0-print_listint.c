98b49514614ca9cd39fd7f95ad41a33c14aeac41
Ashdof <mzigah4@gmail.com>
2023-11-21 21:03:10 +0000

    Task 0 - fix requirement fail issue

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,17 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t nodes = 0;
 
-	while (h)
+	if (h == NULL)
+		return (nodes);
+
+	while (h != NULL)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
 
-	return (i);
+	return (nodes);
 }
