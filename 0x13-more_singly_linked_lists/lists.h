a4e4092596deea60f789795177214f12e5373409
Ashdof <mzigah4@gmail.com>
2023-11-21 21:12:28 +0000

    Task 0 - fix betty coding style issue

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -25,6 +25,7 @@ size_t listint_len(const listint_t *);
 listint_t *add_nodeint(listint_t **, const int);
 listint_t *add_nodeint_end(listint_t **, const int);
 void free_listint(listint_t *);
+void free_listint2(listint_t **);
 
 /* prototypes of helper functions */
 int _putchar(char);
